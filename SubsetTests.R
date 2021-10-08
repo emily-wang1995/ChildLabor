61ec6160c14ec1e58f8e050dcd62a754e1fdb20b
emily-wang1995 <92174563+emily-wang1995@users.noreply.github.com>
2021-10-08 15:51:59 -0700

    Update SubsetTests.R

--- SubsetTests.R
+++ SubsetTests.R
@@ -3,7 +3,7 @@
 source("SubsetData.R")
 
 subsetData("Ethiopia")
-subsetData("Uganda")
+
 
 ethiopia <- read.csv("../../Data/FinalData/ethiopiaFinal.csv",
                      stringsAsFactors = FALSE)
@@ -12,21 +12,7 @@ ethiopiaHousehold <- read.csv("../../Data/FinalData/ethiopiaHousehold.csv",
 ethiopiaIndividual <- read.csv("../../Data/FinalData/ethiopiaIndividual.csv",
                                stringsAsFactors = FALSE)
 
-# Loading in Uganda datasets
-uganda <- read.csv("../../Data/FinalData/ugandaFinal.csv",
-                   stringsAsFactors = FALSE)
-ugandaHousehold <- read.csv("../../Data/FinalData/ugandaHousehold.csv",
-                            stringsAsFactors = FALSE)
-ugandaIndividual <- read.csv("../../Data/FinalData/ugandaIndividual.csv",
-                             stringsAsFactors = FALSE)
-
 
 nrow(ethiopiaHousehold); length(unique(ethiopia$quesID))
 
 nrow(ethiopiaIndividual); nrow(filter(ethiopia, age >= 5 & age <= 17))
-
-
-nrow(ugandaHousehold); length(unique(uganda$quesID))
-
-nrow(ugandaIndividual); nrow(filter(uganda, age >= 5 & age <= 17))
-
